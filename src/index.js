15681384c95b6dbbf644014d450fb40a6e8943cd
Matthew-Burfield <m1.burfield@gmail.com>
2017-06-28 13:07:26 +1000

    fix(random): Updated random function to not use es6 so it will deploy to node v4

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ var uniqueRandomArray = require('unique-random-array')
 var starWarsNames = require('./star-wars-names.json')
 var getRandomItem = uniqueRandomArray(starWarsNames)
 
-const random = num => {
+function random(num) {
   if (num && Number(num) !== 'NaN') {
     array = []
-    for (let i = 0; i < num; i++) {
+    for (var i = 0; i < num; i++) {
       array.push(getRandomItem())
     }
     return array
